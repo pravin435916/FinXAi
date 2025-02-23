9a5674b7d568d0d1e51d9c68708db9e507e2f4b7
Mihir Kalbende <mkmihir16@gmail.com>
2025-02-23 22:45:07 +0530

    added charts

--- client/src/components/chart/page.jsx
+++ client/src/components/chart/page.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const StockChart = () => {
   const [stock, setStock] = useState('AAPL');
